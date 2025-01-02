0668170e2ad9962385666aa413a0548579e5e789
Mathew Moore <mathew.moore@msi-dsl.com>
2025-01-02 13:31:18 +0000

    Copied existing code to draw grid

--- User Controls/SimpleXY.xaml.cs
+++ User Controls/SimpleXY.xaml.cs
@@ -20,9 +20,62 @@ namespace MooreM.UserControls.Charts
     /// </summary>
     public partial class SimpleXY : UserControl
     {
+
+        #region Fields
+        private Color color_BackgroundGrid = System.Windows.Media.Color.FromRgb(192, 192, 192);
+        private Pen? pen_Background_Grid = new Pen(new SolidColorBrush(System.Windows.Media.Color.FromRgb(192, 192, 192)), 1);
+        private Pen pen2 = new Pen(new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 0, 0)), 1);
+        protected Color color_background = Color.FromRgb(230, 230, 120);
+        protected System.Windows.Rect rectangle = new Rect() { Width = 100, Height = 100 };
+        protected int _X;
+        protected int _Y;
+
+        #endregion
+
+        #region Public Properties
+
+        public Color Pen_BackgroundGrid
+        {
+            get { return color_BackgroundGrid; }
+            set
+            {
+                color_BackgroundGrid = value;
+                pen_Background_Grid = new Pen(new SolidColorBrush(color_BackgroundGrid), 1);
+                this.InvalidateVisual();
+            }
+        }
+
+        #endregion
+
+
         public SimpleXY()
         {
             InitializeComponent();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            Draw_BackgroundGrid(drawingContext);
+        }
+
+        protected void Draw_BackgroundGrid(DrawingContext drawingContext)
+        {
+            for (int i = 0; i < Width; i += 10)
+            {
+                Point p1 = new Point(i, 0);
+                Point p2 = new Point(i, Height);
+                drawingContext.DrawLine(pen_Background_Grid, p1, p2);
+
+            }
+
+            for (int i = (int)Height; i > 0; i -= 10)
+            {
+                Point p1 = new Point(0, i);
+                Point p2 = new Point(Width, i);
+                drawingContext.DrawLine(pen_Background_Grid, p1, p2);
+
+            }
         }
     }
 }
