7b593e074f2d80f42acfb3ad542088f054bb49f2
Mathew Moore <mathew.moore@msi-dsl.com>
2025-01-02 13:25:57 +0000

    Created project with simpleXY

---
+++ User Controls/SimpleXY.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MooreM.UserControls.Charts
+{
+    /// <summary>
+    /// Interaction logic for SimpleXY.xaml
+    /// </summary>
+    public partial class SimpleXY : UserControl
+    {
+        public SimpleXY()
+        {
+            InitializeComponent();
+        }
+    }
+}
